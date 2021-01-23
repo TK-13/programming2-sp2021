9a2db364f74d71b6c6928ea468eb22d7889ddf68
bifft2 <bifft95@gmail.com>
2021-01-23 17:49:29 -0600

    add new files

---
+++ Challenges/1-26-21.py
@@ -0,0 +1,6 @@
+"""
+Below, create a function that takes two numbers as arguments and returns their sum
+Do not assume that the user will do a good job of passing in numbers as arguments.
+"""
+
+# erase this and write your code here!
