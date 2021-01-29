5db56fd58376ba0cadae73aacb9525c543982139
tkmuro <tkmuro@icloud.com>
2021-01-29 10:41:48 -0600

    updated from the local version

--- Challenges/1-27-21.py
+++ Challenges/1-27-21.py
@@ -3,4 +3,9 @@ Below, create a function that takes two numbers as arguments and returns their s
 Do not assume that the user will do a good job of passing in numbers as arguments.
 """
 
-# erase this and write your code here!
+def sumthing():
+    b = input("Enter a number: ")
+    c = input("Enter another number: ")
+    print("The sum is", int(b) + int(c))
+
+sumthing()
--- Challenges/1-28-21.py
+++ Challenges/1-28-21.py
@@ -4,4 +4,15 @@ Below, have the user enter a number and have your code generate the Fibonacci se
 If you complete that, also give them the option to input a number N, and generate the sequence to the Nth number.
 """
 
-# erase this and write your code here!
+# 0, 0, 1, 2,
+
+# there was an attempt
+
+number = int(input("Enter a number: "))
+fibonacci = 0
+interval = 1
+
+for i in range(0, 10):
+    fibonacci += interval
+    print(fibonacci)
+    # interval += fibonacci
