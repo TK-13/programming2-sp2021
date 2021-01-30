11d65e07fcd417411e15ed5fff998fe099c5ad0d
tkmuro <tkmuro@icloud.com>
2021-01-30 14:41:48 -0600

    Updated with questions for Ms. Ifft

--- Assignments/intro_questionnaire.py
+++ Assignments/intro_questionnaire.py
@@ -15,16 +15,13 @@ print("\nWelcome to the Get to Know You Questionaire!\n")
 # open the file questionnaire_responses.txt in the same directory as this code, and add their name to it
 questionnaire_file = open('questionnaire_responses.txt', 'w')
 name = input("First, your name please: ")
-questionnaire_file.write(name + "\n\n")
+questionnaire_file.write("Name: " + name + "\n\n")
 
 # create and ask some questions; then print questions and answers to a file.
 questionnaire_simple_questions = [
-    "And what pronouns do you use?",
-    "And can you help me to understand why you took this class?",
-    "What ways do you learn best?\nFor example, with a quiet environment, hands-on, listening, reading, seeing, etc.",
-    "I'm so glad you're here! What are you hoping to get out of this class?",
-    "In your opinion, what qualities does a great teacher have?",
-    "Can you tell me about the piece of code you’ve written that you are most proud of?"
+    "What is your favorite season?",
+    "Do you have any favorite TV shows or books?",
+    "What was your first programming project?"
 ]
 
 for question in questionnaire_simple_questions:
@@ -34,12 +31,11 @@ for question in questionnaire_simple_questions:
     questionnaire_file.write(answer + "\n\n\n")
 
 # ask last question
-last_question = "Last question! Are you an introvert, or extrovert? Or an ambivert maybe? Something else?"
+last_question = "Are you a morning person, a night person, or something else?"
 last_question_dict = {
-    "a": "introvert",
-    "b": "extrovert",
-    "c": "ambivert",
-    "d": "other"
+    "a": "morning person",
+    "b": "night person",
+    "c": "other"
 }
 print("\n" + last_question)
 for key, value in last_question_dict.items():
@@ -50,7 +46,7 @@ introvert_extrovert_explanation = None
 while introvert_extrovert_letter.lower() not in last_question_dict.keys():
     introvert_extrovert_letter = input("Please enter a VALID corresponding letter: ")
 
-if introvert_extrovert_letter.lower() == "d":
+if introvert_extrovert_letter.lower() == "c":
     introvert_extrovert_explanation = input("Help me understand: ")
 
 # write last question and answer to the file
