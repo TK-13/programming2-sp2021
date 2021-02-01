445b51028ce1e16549c730b8e58d2ea26046b076
bifft2 <bifft95@gmail.com>
2021-02-01 09:40:52 -0600

    add 1-28-21 challenge solution

--- Challenges/1-28-21.py
+++ Challenges/1-28-21.py
@@ -4,4 +4,34 @@ Below, have the user enter a number and have your code generate the Fibonacci se
 If you complete that, also give them the option to input a number N, and generate the sequence to the Nth number.
 """
 
-# erase this and write your code here!
+num = int(input("What number would you like your fibonacci sequence to go up to? "))
+
+fib_1 = 0
+fib_2 = 1
+
+print(fib_1)
+print(fib_2)
+
+while fib_2 + fib_1 < num:
+    print(fib_1 + fib_2)
+    temp_fib_1 = fib_1
+    fib_1 = fib_2
+    fib_2 += temp_fib_1
+
+
+n = int(input("How many fibonacci numbers would you like to see? "))
+
+fib_1 = 0
+fib_2 = 1
+
+if n >= 1:
+    print(fib_1)
+
+if n >= 2:
+    print(fib_2)
+
+for i in range(n-2):
+    print(fib_1 + fib_2)
+    temp_fib_1 = fib_1
+    fib_1 = fib_2
+    fib_2 += temp_fib_1
