6a9385e4486059492d43af48beaa05d96bc8234c
bifft2 <bifft95@gmail.com>
2021-02-01 20:56:31 -0600

    add 2-2-21.py challenge and the solution to 2-1-21.py challenge

---
+++ Challenges/2-2-21.py
@@ -0,0 +1,8 @@
+"""
+Below, create a function that takes in an age and returns that person's age in days.
+(You can ignore the days since their last birthday)
+If you finish that.... don't forget about leap years!
+Hint: there may be a library to help you out on this.
+"""
+
+# erase this and write your code here!
