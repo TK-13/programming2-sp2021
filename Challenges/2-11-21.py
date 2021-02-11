110b848994afff8f900e916e40c5d8917f85d3b4
tkmuro <tkmuro@icloud.com>
2021-02-11 12:48:41 -0600

    ...an attempt was made.

--- Challenges/2-11-21.py
+++ Challenges/2-11-21.py
@@ -3,3 +3,13 @@ Reverse a String - See if you can write a function that takes in a string, and r
 If you complete that, tell the user if their word is a palindrome.
 A hint is that strings can be manipulated similarly to lists :)
 """
+
+
+def reversal(enter):
+    reversed_string = ""
+    for i in range(len(enter)):
+        reversed_string += enter[int(len(enter) - i)]
+    print(reversed_string)
+
+
+reversal("Hello")
