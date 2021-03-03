166caaad987415707442b10b94cb42f0478d8788
bifft2 <bifft95@gmail.com>
2021-03-03 17:13:30 -0600

    add 3-4 challenge

---
+++ Challenges/3-4-21.py
@@ -0,0 +1,24 @@
+"""
+Dictionary extraction - extract the values out of the slogans dict which have keys that appear in keys_to_extract
+
+If you complete that, try to make a new dictionary out of the keys and values you extract, and remove them from
+the slogans dictionary.
+"""
+
+slogans = {
+    "Nike": "Just Do It",
+    "Wheaties": "Breakfast of Champions",
+    "Dunkin' Donuts": "America Runs on Dunkin'",
+    "McDonald's": "I’m Lovin’ It",
+    "Skittles": "Taste the rainbow",
+    "Capital One": "What's in your wallet?",
+    "Subway": "Eat Fresh",
+    "Goldfish": "The snack that smiles back",
+    "Toyota": "Let's go places",
+    "Rice Krispies": "Snap! Crackle! Pop!",
+}
+
+keys_to_extract = ["McDonald's", "Nike", "Toyota"]
+
+# insert your code here :)
+
