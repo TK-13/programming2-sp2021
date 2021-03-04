a8fdbb2c738025bbca3f7b9aa98a917c53b76675
bifft2 <bifft95@gmail.com>
2021-03-04 15:00:12 -0600

    in class dictionaries assignment

---
+++ InClassActivities/dictionaries.py
@@ -0,0 +1,5 @@
+# 1. go find a cool csv that you're interested in (kaggle is a great place to look)
+# 2. think about one question you'd like to answer, and write down required steps to answer it
+# 3. read in csv file (I challenge you to store it in a nested dict!)
+# 4. translate steps from 2 into code and see if you can answer your question
+
