d6d78907f38b3dbf9726a77d74782e2e5cfbbdfc
tkmuro <tkmuro@icloud.com>
2021-03-09 09:45:19 -0600

    More progress, cancelling and reschedule system in progress. Improve cancel system, so that there's no blank dictionary floating in schedule.

--- Labs/Lab1/Lab_1.py
+++ Labs/Lab1/Lab_1.py
@@ -3,26 +3,40 @@ import csv
 
 schedule_reader = open('/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab1/my_schedule.csv', 'r')
 dict_reader = csv.DictReader(schedule_reader)
-schedule_list = []
-for row in dict_reader:
-    # print(row)
-    schedule_list.append(row)
+schedule_list = list(csv.DictReader(schedule_reader))
 
-# for i in schedule_list:
-#     print(i)
-#     print(type(i))
+print("Check 1 ", schedule_list)
 
-# print(schedule_list[1])
-# for k, v in schedule_list[1].items():
-#     print(k, v)
+new_start = ""
+conflict_list = []
 
-def conflict_check(date, time):
-    for row in range(len(schedule_list)):
-        if schedule_list[row]['Date'] == str(date):
-            if schedule_list[row]['Start Time'] == time:
-                print("That time is not available.")
-            else:
-                print("That time is open.")
+
+def conflict_check(date):
+    global conflict_list
+    for i in range(len(schedule_list)):
+        start_time = schedule_list[i]['Start Time']
+        conflict_list.append(start_time)
+        if schedule_list[i]['Date'] == str(date):
+            if conflict_list.count(start_time) >= 2:
+                print("{0} is not available.".format(start_time))
+                res_not_done = True
+                while res_not_done:
+                    reschedule = input("Would you like to cancel or reschedule {0}? (r/c) ".format(schedule_list[i]['Event']))
+                    if reschedule.lower() == "r":
+                        res_not_done = False
+                        global new_start
+                        global new_end
+                        new_start = input("New start time: ")
+                        schedule_list[i]["Start Time"] = new_start
+                        new_end = input("New end time: ")
+                        schedule_list[i]["End Time"] = new_end
+                        break
+                    elif reschedule.lower() == "c":
+                        res_not_done = False
+                        schedule_list[i].remove()
+
+
+print("Check 2 ", schedule_list)
 
 # date = input("What row do you want to check? ")
 # check_time = input("What time do you want to check? ")
@@ -33,5 +47,13 @@ def conflict_check(date, time):
 # print(schedule_list[0]['Start Time'])
 # print(dict_reader["03/08/2021"])
 
+conflict_check('03/08/2021')
+# print(schedule_list)
+# print(new_start)
 
-conflict_check('03/08/2021', '8:10')
+schedule_csv = open('/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab1/my_schedule.csv', 'w')
+writer = csv.writer(schedule_csv)
+writer.writerow(["Date", "Event", "Start Time", "End Time"])
+for x in range(len(schedule_list)):
+    # print(schedule_list[x]["Event"])
+    writer.writerow([schedule_list[x]["Date"], schedule_list[x]["Event"], schedule_list[x]["Start Time"], schedule_list[x]["End Time"]])
