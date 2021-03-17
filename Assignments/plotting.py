ca915c0980452bd1569fb73cc66bba34941783a2
bifft2 <bifft95@gmail.com>
2021-03-17 13:21:56 -0500

    add plotting.py assignment

---
+++ Assignments/plotting.py
@@ -0,0 +1,18 @@
+'''
+CTA Ridership (22pts)
+Get the csv from the following data set.
+https://data.cityofchicago.org/api/views/w8km-9pzd/rows.csv?accessType=DOWNLOAD
+This shows CTA ridership by year going back to the 80s
+It has been updated with 2018 data, but not yet with anything after unfortunately
+1  Download the dataset and read in the file.
+2  Retrieve the bus, rail, and total data for the last 15 years from the csv file. (5pts)
+3  Make a line plot of rail usage for the last 15 years of data.  (year on x axis, and ridership on y) (3pts)
+4  Plot bus usage for the same years as a second line on your graph. (3pts)
+5  Plot total usage on a third line on your graph. (3pts)
+6  Add a title and label your axes. (3pts)
+7  Add a legend to show data represented by each of the three lines. (3pts)
+8  What trend or trends do you see in the data?  Offer a hypotheses which might explain the trend(s). Just add a comment here to explain. (2pts)
+'''
+
+
+
