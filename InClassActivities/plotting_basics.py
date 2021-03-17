0b9f1a2a2cfa1b0ff6526a821984a28f7d2ad3c4
bifft2 <bifft95@gmail.com>
2021-03-17 13:19:57 -0500

    add plotting_basics.py activity

---
+++ InClassActivities/plotting_basics.py
@@ -0,0 +1,21 @@
+"""
+Your task is to recreate the plot here: https://drive.google.com/file/d/1YLyShCmcvyBWHNfCzJx7Nz_dFvcAzBCY/view?usp=sharing
+
+Steps:
+1. Create a list of the squared x_values
+2. Create a list of the cubed x_values
+3. Create a green linear line with the label linear
+4. Create an orange squared line with the label squares
+5. Create a blue cubed line with the label cubes
+6. Title the plot Simple Plot
+7. Label the x axis Xs
+8. Label the y axis Ys
+9. Add the legend, and make sure the labels are correct
+10. Show the plot!
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_values = np.linspace(0, 2, 100)
+
