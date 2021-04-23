adf21e3cbda555cbf33e0d0bf391123ee0f2f465
tkmuro <tkmuro@icloud.com>
2021-04-23 11:46:44 -0500

    Graph label improvements, added the circle mapping to the main file (originally in a different file for easy tinkering).

--- Labs/Lab2/Lab_2.py
+++ Labs/Lab2/Lab_2.py
@@ -1,10 +1,13 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
+import folium
 
+# Empty lists, which will be rewritten later. total_years is specified, because the suicide dataset contains data for
+# a specific set of years.
 country_names = []
 total_events = []
 total_years = [2000, 2010, 2015, 2016]
+center_coordinates = [37.5131, 122.1204]
 
 
 # This function compiles a chronological list of the total events of the specified country. This is used to efficiently
@@ -49,6 +52,7 @@ def user_input(message, param_list):
 # Using read_data() to read the respective datasets into dictionaries
 new_suicide_dataset = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/Age-standardized suicide rates.csv")
 coordinate_dataset = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/countries.csv")
+place_list = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/countries.csv")
 conflict_dataset = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/gdelt_processor_friendly.csv")
 # The 'processor friendly' version only has data beyond the year 2000, since that's roughly the range of the suicide
 # data. This way, the program can spend less time looking through all 176k lines of the original dataset, while still
@@ -83,7 +87,7 @@ my_dict {
 '''
 
 
-# Making Dictionaries for each country
+# Making Dictionaries for each country, all nested within an overall dictionary.
 country_dict = {}
 for i in new_suicide_dataset:
     if i["Country"] not in country_names:
@@ -98,6 +102,8 @@ for i in new_suicide_dataset:
 # for k, v in country_dict.items():
 #     print(k, "\n", v)
 
+# Graphing suicide rates and total conflicts over time for any arbitrary country. Graphing 2 types of data
+# for every country would be a nightmare, so I had to go one at a time.
 plt.figure(0, tight_layout=True)
 print(country_names)
 target = user_input("Select a country: ", country_names)
@@ -105,18 +111,43 @@ target = user_input("Select a country: ", country_names)
 x_key = 'Years'
 y_key_1 = 'Suicide Rates'
 y_key_2 = 'Events'
-x_vals = [x for x in country_dict[target][x_key]]
-y1_vals = [y*(10**4) for y in country_dict[target][y_key_1]]
-y2_vals = [y for y in country_dict[target][y_key_2]]
+x_values = [x for x in country_dict[target][x_key]]
+y1_values = [y * (10 ** 4) for y in country_dict[target][y_key_1]]
+# Suicide rates are multiplied like so for easier reading on the graph. Otherwise it looked horizontal.
+# consider not manipulating the data.
+y2_values = [y for y in country_dict[target][y_key_2]]
 
-plt.plot(x_vals, y1_vals, color="red", label='Suicide Rates')
-plt.plot(x_vals, y2_vals, color="blue", label='Total Events')
-plt.title('Simple Plot')
+plt.plot(x_values, y1_values, color="red", label='Suicide Rates')
+plt.plot(x_values, y2_values, color="blue", label='Total Events')
+plt.title('Suicide Rates and Total Events of ' + target + ' over Time')
 plt.xlabel(x_key)
-plt.ylabel("Total Events (10^6)\nand Suicide Rates (10^6 for easy viewing)")
+plt.ylabel("Total Events (10^6) and\nSuicide Rates (10^4 for easy viewing)")
 plt.legend()
 # plt.xticks(rotation=75, size=5)
 # plt.yticks(size=5)
-
 plt.show()
 
+# Finding correlation between conflict and suicides
+# for every country:
+# find overall change in conflicts, suicides
+# the smaller the difference, the bigger the radius
+# the greater the difference, the smaller the radius (correlation)
+
+# Mapping correlation between conflict and suicides geographically.
+circle_map = folium.Map(location=center_coordinates, zoom_start=2)
+for place in place_list:
+    if place['latitude'] != "" and place['longitude'] != "":
+        place['latitude'] = float(place['latitude'])
+        place['longitude'] = float(place['longitude'])
+
+    coordinates = (place['latitude'], place['longitude'])
+
+    folium.CircleMarker(
+        radius=100,
+        location=coordinates,
+        popup=place['name'],
+        color="crimson",
+        fill=False,
+    ).add_to(circle_map)
+
+circle_map.save('my_map.html')
