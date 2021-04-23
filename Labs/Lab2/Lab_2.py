2e6582d49be2d4361ecd5394684d5fafb5919de5
tkmuro <tkmuro@icloud.com>
2021-04-22 21:28:57 -0500

    Figured out how to populate the lists again. Found a better dataset, reworked a lot. Solid start with graphing.

--- Labs/Lab2/Lab_2.py
+++ Labs/Lab2/Lab_2.py
@@ -2,15 +2,49 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_raw = open("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/gdelt_conflict_1_0.csv")
-data_reader = csv.DictReader(data_raw)
-conflict_dataset = list(data_reader)
-data_raw.close()
+country_names = []
+total_events = []
+total_years = [2000, 2010, 2015, 2016]
 
-data_raw = open("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/suicides.csv")
-data_reader = csv.DictReader(data_raw)
-suicide_dataset = list(data_reader)
-data_raw.close()
+
+# This function compiles a chronological list of the total events of the specified country. This is used to efficiently
+# populate the event information for each country.
+def populate_events(country):
+    year_tracker = []
+    events = []
+    for i in conflict_dataset:
+        if i["CountryName"] == country and int(i['Year']) in total_years and i["Year"] not in year_tracker:
+            events.append(int(i["TotalEvents"]))
+            year_tracker.append(i["Year"])
+    return events
+
+
+# This function returns a chronological list of the suicide rates of the specified country.
+def populate_suicides(country):
+    suicide_rates = []
+    for i in new_suicide_dataset:
+        if i["Country"] == country and i["Sex"] == " Both sexes":
+            for t in total_years:
+                suicide_rates.append(float(i[str(t)]))
+    return suicide_rates
+
+
+# This function just makes it easier to put the datasets into a dictionary.
+def read_data(path):
+    data_raw = open(path)
+    data_reader = csv.DictReader(data_raw)
+    file = list(data_reader)
+    data_raw.close()
+    return file
+
+
+# Using read_data() to read the respective datasets into dictionaries
+new_suicide_dataset = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/Age-standardized suicide rates.csv")
+conflict_dataset = read_data("/Users/tkmuro/PycharmProjects/tkProgramming/Labs/Lab2/gdelt_processor_friendly.csv")
+# The 'processor friendly' version only has data beyond the year 2000, since that's roughly the range of the suicide
+# data. This way, the program can spend less time looking through all 176k lines of the original dataset, while still
+# having to sort through and find the right values, as it normally would.
+
 
 '''
 Example:
@@ -39,68 +73,41 @@ my_dict {
 }
 '''
 
-# years = []
-# for yr in suicide_dataset:
-#     if yr["year"] not in years:
-#         years.append(yr["year"])
-# years.sort()
 
-country_names = []
-total_events = []
-total_years = []
-
-
-# This function compiles a chronological list of the total events of the specified country. This is used to efficiently
-# populate the event information for each country.
-def populate_events(country):
-    year_tracker = []
-    events = []
-    for i in conflict_dataset:
-        if i["CountryName"] == country and i["Year"] not in year_tracker:
-            events.append(int(i["TotalEvents"]))
-            year_tracker.append(i["Year"])
-    return events
-
-
-conflict_dict = {}
-for i in conflict_dataset:
-    # List of all years
-    if i["Year"] not in total_years:
-        total_years.append(i["Year"])
-
-    # Making Dictionaries for each country
-    if i["CountryName"] not in country_names:
-        country_names.append(i["CountryName"])
-        key = str(i["CountryName"])
-        conflict_dict[key] = {
+# Making Dictionaries for each country
+country_dict = {}
+for i in new_suicide_dataset:
+    if i["Country"] not in country_names:
+        country_names.append(i["Country"])
+        key = str(i["Country"])
+        country_dict[key] = {
             "Years": total_years,
-            "Suicides": [],
+            "Suicide Rates": populate_suicides(key),
             "Events": populate_events(key)
         }
 
-for k, v in conflict_dict.items():
-    print(k, "\n", v)
+# for k, v in country_dict.items():
+#     print(k, "\n", v)
 
-# plt.figure(0, tight_layout=True)
-#
-# x_vals = [x for x in country_names]
-# y_vals = [y for y in total_events]
-#
-# plt.bar(x_vals, y_vals)
+plt.figure(0, tight_layout=True)
+target = "China"
+x_key = 'Years'
+y_key_1 = 'Suicide Rates'
+y_key_2 = 'Events'
+
+print(country_dict[target][y_key_1])
+
+x_vals = [x for x in country_dict[target][x_key]]
+y1_vals = [y for y in country_dict[target][y_key_1]]
+y2_vals = [y for y in country_dict[target][y_key_2]]
+
+plt.plot(x_vals, y1_vals, color="red")
+plt.plot(x_vals, y2_vals, color="blue")
+plt.title('Simple Plot')
+plt.xlabel(x_key)
+plt.ylabel("Placeholder")
 # plt.xticks(rotation=75, size=5)
 # plt.yticks(size=5)
-#
-#
-# plt.show()
 
+plt.show()
 
-# for s in suicide_dataset:
-#     print(s['year'])
-
-# suicide_data = {}
-# c_names = []
-# suicide_rates = []
-# for s in suicide_dataset:
-#     if s['country'] not in c_names:
-#         c_names.append(s['country'])
-# print(c_names)
